148c5b4ebd0130b2db16f317e46abc066877b49c
Angel <angel.adael@hotmail.com>
2017-03-27 20:51:20 -0700

    Termine esta madre (Fuck You Bressenham)

--- Lineas de Bresenhaimsd/Lineas de Bresenhaimsd/Form1.Designer.cs
+++ Lineas de Bresenhaimsd/Lineas de Bresenhaimsd/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -37,9 +40,9 @@
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 547);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 574);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(645, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(701, 22);
             this.statusStrip1.TabIndex = 0;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -49,15 +52,43 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
             this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 537);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(254, 511);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(360, 511);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(645, 569);
+            this.ClientSize = new System.Drawing.Size(701, 596);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.statusStrip1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
@@ -72,6 +103,9 @@
 
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Lineas de Bresenhaimsd/Lineas de Bresenhaimsd/Form1.cs
+++ Lineas de Bresenhaimsd/Lineas de Bresenhaimsd/Form1.cs
@@ -24,7 +24,50 @@ namespace Lineas_de_Bresenhaimsd
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            
+            //Majorita
+            //DibujarCoordenadas(370, 430, 120, 210);
+            //DibujarCoordenadas(120, 210, 150, 90);
+            //DibujarCoordenadas(150, 90, 220, 30);
+            //DibujarCoordenadas(220, 25, 350, 100);
+            //DibujarCoordenadas(350, 100, 460, 30);
+            //DibujarCoordenadas(460, 30, 553, 75);
+            //DibujarCoordenadas(553, 75, 590, 188);
+            //DibujarCoordenadas(590, 188, 371, 432);
+
+            //DibujarCoordenadas(195, 28, 250, 28);
+            //DibujarCoordenadas(250, 28, 210, 1);
+            //DibujarCoordenadas(210, 1, 195, 28);
+
+            //DibujarCoordenadas(437, 7, 510, 7);
+            //DibujarCoordenadas(510, 7, 626, 1);
+            //DibujarCoordenadas(626, 1, 437, 7);
+
+            //DibujarCoordenadas(272, 239 ,216 ,170 );
+            //DibujarCoordenadas(216,170 ,258 ,114 );
+            //DibujarCoordenadas(258,114 ,331 ,178 );
+            //DibujarCoordenadas(331,178 ,331 ,178 );
+
+            //DibujarCoordenadas(441,173 ,450 ,124 );
+            //DibujarCoordenadas(450,124 ,517 ,183 );
+            //DibujarCoordenadas(517,183 ,469 ,225 );
+
+            //Relojkito de Arenita
+            //DibujarCoordenadas(250, 430, 450, 430);
+            //DibujarCoordenadas(450, 430, 250, 230);
+            //DibujarCoordenadas(230, 230, 450, 230);
+            //DibujarCoordenadas(450, 230, 250, 430);
+
+            //Reliquias de la muerte
+            DibujarCirculo(350, 300, 100);
+
+            DibujarCoordenadas(100, 400, 600, 400);
+            DibujarCoordenadas(600, 400, 350, 200);
+            DibujarCoordenadas(350, 150, 100, 400);
+
+            DibujarCoordenadas(350, 400, 350, 150);
+
+
+
         }
 
         private void DibujarLinea(int xfinal, int yfinal, int inicialx, int inicialy, Color color, int grosor = 1)
@@ -115,8 +158,8 @@ namespace Lineas_de_Bresenhaimsd
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
             int[] x = new int[2];
-            x[0] = e.X;
-            x[1] = e.Y;
+            x[0] = Convert.ToInt32(e.X);
+            x[1] = Convert.ToInt32(e.Y);
 
             listaPosiciones.Add(x);
 
@@ -144,9 +187,71 @@ namespace Lineas_de_Bresenhaimsd
             toolStripStatusLabel1.Text = coordenadas;
         }
 
-        private void DibujarSombra()
+        private void button1_Click(object sender, EventArgs e)
         {
-            
+            int[] x = new int[2];
+            x[0] = Convert.ToInt32(textBox1.Text);
+            x[1] = Convert.ToInt32(textBox2.Text);
+
+            listaPosiciones.Add(x);
+
+
+            try
+            {
+                int x1 = listaPosiciones[actual][0] - listaPosiciones[actual - 1][0];
+                int y1 = listaPosiciones[actual - 1][1] - listaPosiciones[actual][1];
+                int x2 = listaPosiciones[actual - 1][0];
+                int y2 = listaPosiciones[actual - 1][1];
+                DibujarLinea(x1, y1, x2, y2, Color.Crimson);
+            }
+            catch (Exception)
+            {
+
+            }
+
+            actual++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DibujarCoordenadas(int x1, int y1, int x2, int y2)
+        {
+            int a = x2 - x1;
+            int b = y1 - y2;
+            DibujarLinea(a, b, x1, y1, Color.Crimson);
+        }
+
+        private void DibujarCirculo(int x, int y, int r)
+        {
+            int x1 = 0;
+            int y1 = r;
+            int d = 3 - 2*r;
+            Graphics graphicsObj = CreateGraphics();
+            Pen pluma = new Pen(Color.Crimson, 1);
+            while (x1 <= y1)
+            {
+                graphicsObj.DrawRectangle(pluma, x + x1, y + y1, 1, 1);
+                graphicsObj.DrawRectangle(pluma, x - x1, y + y1, 1, 1);
+                graphicsObj.DrawRectangle(pluma, x + x1, y - y1, 1, 1);
+                graphicsObj.DrawRectangle(pluma, x - x1, y - y1, 1, 1);
+                graphicsObj.DrawRectangle(pluma, x + y1, y + x1, 1, 1);
+                graphicsObj.DrawRectangle(pluma, x - y1, y + x1, 1, 1);
+                graphicsObj.DrawRectangle(pluma, x + y1, y - x1, 1, 1);
+                graphicsObj.DrawRectangle(pluma, x - y1, y - x1, 1, 1);
+                if (d < 0)
+                {
+                    d += 4*x1 + 6;
+                }
+                else
+                {
+                    d += 4*(x1 - y1) + 10;
+                    y1--;
+                }
+                x1++;
+            }
         }
     }
 }
